add1b84a096ff09f5301d990572c574fd3fa6151
佐藤良昭 <yoshiprograming@gmail.com>
2020-04-16 17:46:42 +0900

    使用をみたす機能を実装（見た目は未完成）

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,74 +1,57 @@
 class HomesController < ApplicationController
   before_action :set_home, only: [:show, :edit, :update, :destroy]
 
-  # GET /homes
-  # GET /homes.json
+  
   def index
     @homes = Home.all
   end
 
-  # GET /homes/1
-  # GET /homes/1.json
+ 
   def show
   end
 
-  # GET /homes/new
+ 
   def new
     @home = Home.new
+    2.times {@home.detail.build}
   end
 
-  # GET /homes/1/edit
+
   def edit
+    @home.detail.build
   end
 
-  # POST /homes
-  # POST /homes.json
   def create
     @home = Home.new(home_params)
-
-    respond_to do |format|
       if @home.save
-        format.html { redirect_to @home, notice: 'Home was successfully created.' }
-        format.json { render :show, status: :created, location: @home }
+        redirect_to @home, notice: 'Home was successfully created.'
       else
-        format.html { render :new }
-        format.json { render json: @home.errors, status: :unprocessable_entity }
+        render :new 
       end
-    end
   end
 
-  # PATCH/PUT /homes/1
-  # PATCH/PUT /homes/1.json
+
   def update
-    respond_to do |format|
       if @home.update(home_params)
-        format.html { redirect_to @home, notice: 'Home was successfully updated.' }
-        format.json { render :show, status: :ok, location: @home }
+        redirect_to @home, notice: 'Home was successfully updated.' 
       else
-        format.html { render :edit }
-        format.json { render json: @home.errors, status: :unprocessable_entity }
+        render :edit
       end
-    end
   end
 
-  # DELETE /homes/1
-  # DELETE /homes/1.json
+
   def destroy
     @home.destroy
-    respond_to do |format|
-      format.html { redirect_to homes_url, notice: 'Home was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+      redirect_to homes_url, notice: 'Home was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_home
       @home = Home.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def home_params
-      params.require(:home).permit(:name, :money, :address, :age, :context)
+      params.require(:home).permit(:name, :money, :address, :age, :context, detail_attributes: [:route_name, :station_name, :distance, :id, :_destroy])
     end
-end
+  end
